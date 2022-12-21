e50665823100ca548a1c1ca1974e66ea6f7c2e39
Nikhil Satyam <110683580+Nikh9123@users.noreply.github.com>
2022-12-21 16:13:22 +0530

    Update app.js

--- unconventional calculator/assets/scripts/app.js
+++ unconventional calculator/assets/scripts/app.js
@@ -31,49 +31,48 @@ function writeToLog(
 
 function calculateResult(calculationType) {
   const enteredNumber = getUserNumberInput();
-  if (calculationType !== 'ADD' &&
-    calculationType !== 'SUBTRACT'
-    && calculationType !== 'MULTIPLY'
-    && calculationType !== 'DIVIDE'
-    || !enteredNumber) {
+  if (
+    calculationType !== 'ADD' &&
+    calculationType !== 'SUBTRACT' &&
+    calculationType !== 'MULTIPLY' &&
+    calculationType !== 'DIVIDE' &&
+    calculationType !== 'EXPONENT' ||
+    !enteredNumber
+  ) {
     return;
   }
+
   // if (
   //   calculationType === 'ADD' ||
-  //   calculationType === 'SUBTRACT'
-  //   || calculationType === 'MULTIPLY'
-  //   || calculationType === 'DIVIDE'
+  //   calculationType === 'SUBTRACT' ||
+  //   calculationType === 'MULTIPLY' ||
+  //   calculationType === 'DIVIDE'
   // ) {
 
-    const initialResult = currentResult;
-    let mathOperator;
-    if (calculationType === 'ADD') {
-      currentResult += enteredNumber;
-      mathOperator = '+';
-    }
-    else if (calculationType === 'SUBTRACT') {
-      currentResult -= enteredNumber;
-      mathOperator = '-';
-    }
-    else if (calculationType === 'MULTIPLY') {
-
-      currentResult *= enteredNumber;
-      mathOperator = '*';
-    }
-    else if (calculationType === 'DIVIDE') {
-      currentResult /= enteredNumber;
-      mathOperator = '/';
-    }
-
-
-    createAndWriteOutput(mathOperator, initialResult, enteredNumber);
-    writeToLog(calculationType, initialResult, enteredNumber, currentResult);
+  const initialResult = currentResult;
+  let mathOperator;
+  if (calculationType === 'ADD') {
+    currentResult += enteredNumber;
+    mathOperator = '+';
+  } else if (calculationType === 'SUBTRACT') {
+    currentResult -= enteredNumber;
+    mathOperator = '-';
+  } else if (calculationType === 'MULTIPLY') {
+    currentResult *= enteredNumber;
+    mathOperator = '*';
+  } else if (calculationType === 'DIVIDE') {
+    currentResult /= enteredNumber;
+    mathOperator = '/';
+  }
+  else if (calculationType === 'EXPONENT') {
+    currentResult **= enteredNumber;
+    mathOperator = '^';
   }
 
-
-
-// }
-
+  createAndWriteOutput(mathOperator, initialResult, enteredNumber);
+  writeToLog(calculationType, initialResult, enteredNumber, currentResult);
+  // }
+}
 
 function add() {
   calculateResult('ADD');
@@ -84,14 +83,18 @@ function subtract() {
 }
 
 function multiply() {
-  calculateResult("MULTIPLY")
+  calculateResult('MULTIPLY');
 }
 
 function divide() {
-  calculateResult("DIVIDE")
+  calculateResult('DIVIDE');
+}
+function exponenet() {
+  calculateResult('EXPONENT');
 }
 
 addBtn.addEventListener('click', add);
 subtractBtn.addEventListener('click', subtract);
 multiplyBtn.addEventListener('click', multiply);
 divideBtn.addEventListener('click', divide);
+exponentBtn.addEventListener('click' , exponenet);
